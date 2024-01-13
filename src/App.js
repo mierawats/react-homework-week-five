08d24dde4564f768149a91de2200782d11abe91b
mierawats <azmiera.azmi.expert@gmail.com>
2024-01-13 18:49:37 +0800

    Made the basic structure of app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center mt-3 fw-bolder">Hello Weather</h1>
+      <Weather />
+      <footer className="footer text-center m-3">
+        Coded by {""}
+        <a
+          href="https://www.linkedin.com/in/azmiera-expert/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Miera Azmi
+        </a>
+        , is an open-sourced on {""}
+        <a
+          href="https://github.com/mierawats/react-homework-week-five"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        , and hosted by {""}
+        <a
+          href="https://vercel.com/mieras-projects-043d06f5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Vercel
+        </a>
+        .
+      </footer>
     </div>
   );
 }
