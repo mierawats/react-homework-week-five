f86f3c976619aec7c285bdfc0a915a3aa88a9893
mierawats <azmiera.azmi.expert@gmail.com>
2024-01-13 12:12:36 +0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Weather</div>;
+  return (
+    <div className="App">
+      <h1 className="text-center mt-3 fw-bolder">Hello Weather</h1>
+    </div>
+  );
 }
 
 export default App;
