5da54ab519ecc4df85e096c0305b5af1196dbcd4
mierawats <azmiera.azmi.expert@gmail.com>
2024-01-14 17:29:25 +0800

    Added Fahrenheit to temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,12 +17,7 @@ export default function WeatherInfo(props) {
           <img src={props.data.iconUrl} alt={props.data.description} />
         </div>
         <div className="col-4 text-start m-auto">
-          <div className="tempdata">
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
-          </div>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6 m-auto">
           <ul>
